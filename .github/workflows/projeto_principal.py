5b0d19c5a2d1b0e5db1cf05a1a21a9d858342a3f
jonattanOlivera <jonattangolivera@gmail.com>
2024-12-30 22:33:11 -0300

    projeto_principal.py

--- .github/workflows/projeto_principal.py
+++ .github/workflows/projeto_principal.py
@@ -5,3 +5,4 @@ df_reviews = pd.read_csv(".github/workflows/dados_projeto/customer reviews.csv")
 df_top_100_books = pd.read_csv(".github/workflows/dados_projeto/Top-100 Trending Books.csv")
 
 df_reviews
+df_top_100_books
