c4477c1008f466dfeb31eaf0ea2bc88113fe15df
jonattanOlivera <jonattangolivera@gmail.com>
2024-12-31 00:16:46 -0300

    projeto_principal.py

--- .github/workflows/projeto_principal.py
+++ .github/workflows/projeto_principal.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 df_reviews = pd.read_csv(".github/workflows/dados_projeto/customer reviews.csv")
 df_top_100_books = pd.read_csv(".github/workflows/dados_projeto/Top-100 Trending Books.csv")
-
+st.title("Top 100 Bestselling Book Reviews on Amazon")
 price_max = df_top_100_books["book price"].max()  #crio um var e defino que ela recebera um metodo pandas que lee a coluna de preço e logo utilizo um metodo para procurar com maior valor, faço o mesmo com a metodo min
 price_min = df_top_100_books["book price"].min()
 max_price = st.sidebar.slider("Preços dos Livros",price_min,price_max,price_max) #siderbar 
