05927d19be6875cb3785f73869deaf428d067e9e
jonattanOlivera <jonattangolivera@gmail.com>
2024-12-30 22:24:54 -0300

    projeto_principal.py

--- .github/workflows/projeto_principal.py
+++ .github/workflows/projeto_principal.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import panda as pd
+import pandas as pd
 
 df_reviews = pd.read_csv("dados_projeto/customer reviews.csv")
 df_top_100_books = pd.read_csv("dados_projeto/Top-100 Trending Books.csv")
