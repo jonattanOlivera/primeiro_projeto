9060e60060eda12cd55d4234678be91a07d4d5d3
jonattanOlivera <jonattangolivera@gmail.com>
2025-01-02 23:01:57 -0300

    projeto_principal.py

--- .github/workflows/projeto_principal.py
+++ .github/workflows/projeto_principal.py
@@ -1,11 +1,11 @@
-import streamlit as st
-import pandas as pd
+import streamlit as st #Cria o App
+import pandas as pd #manipula dados e tabelas
 
 df_reviews = pd.read_csv(".github/workflows/dados_projeto/customer reviews.csv")
 df_top_100_books = pd.read_csv(".github/workflows/dados_projeto/Top-100 Trending Books.csv")
 st.title("Top 100 Bestselling Book Reviews on Amazon")
 price_max = df_top_100_books["book price"].max()  #crio um var e defino que ela recebera um metodo pandas que lee a coluna de preço e logo utilizo um metodo para procurar com maior valor, faço o mesmo com a metodo min
 price_min = df_top_100_books["book price"].min()
-max_price = st.sidebar.slider("Preços dos Livros",price_min,price_max,price_max) #siderbar 
-df_books = df_top_100_books[df_top_100_books["book price"] <= max_price]
-df_books
+max_price = st.sidebar.slider("Preços dos Livros",price_min,price_max,price_max) #siderbar https://docs.streamlit.io/develop/api-reference/widgets/st.slider
+df_books = df_top_100_books[df_top_100_books["book price"] <= max_price] # Variavel procura dentro da coluna book price o maior o igual valor. isso o pandas manipula a tapela 
+
